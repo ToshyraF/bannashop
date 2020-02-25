b7c191ec3994a6f3de2f7771f725621d5b652769
JIRAYUD.WI <jirayud.wi@GLSCISLT03.local>
2020-02-25 22:24:22 +0700

    init

--- banna/settings.py
+++ banna/settings.py
@@ -123,6 +123,6 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
