ccc80f0d7a0f139a54e3c4349b3f7a16c5fba3ed
JIRAYUD.WI <jirayud.wi@GLSCISLT03.local>
2020-02-25 22:23:24 +0700

    init

--- banna/settings.py
+++ banna/settings.py
@@ -119,4 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
