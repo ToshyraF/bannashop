690190daccd6e4ee9131f8b08dfe5505dad645f7
JIRAYUD.WI <jirayud.wi@GLSCISLT03.local>
2020-03-06 00:13:33 +0700

    Edit add order

--- static/js/shop.js
+++ static/js/shop.js
@@ -1,5 +1,18 @@
 $(function(){
-    $('.modal').modal();
-    $('.collapsible').collapsible();
-    $(".js-example-tags").select2();
+    
+   
+
+    // mainmodal
+    $('#mainmodal').on('shown.bs.modal', function () {
+        // $('#modal1').css("filter","blur(5px)");
+        })
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#mainmodal').css("filter","blur(5px)");
+    })
+    $('#myModal').on('hidden.bs.modal', function () {
+        $('#mainmodal').css("filter","blur(0px)");
+    })
+
+        $('.collapsible').collapsible();
+        $(".js-example-tags").select2();
 })
